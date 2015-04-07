ab6a9a454d54ab4221511d6eb78a3028b9a745bd
Shaun Williams <shaun.e.williams@gmail.com>
2015-04-07 02:06:03 -0500

    remove jquery dependency (thanks http://youmightnotneedjquery.com/)

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [figwheel "0.2.5"]
                  [cljsjs/hammer "2.0.4-4"]
-                 [cljsjs/jquery "1.9.0-0"]
                  [markdown-clj "0.9.65"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
--- src/solar_system_of_js/caption.cljs
+++ src/solar_system_of_js/caption.cljs
@@ -1,24 +1,21 @@
 (ns solar-system-of-js.caption
   (:require
     [solar-system-of-js.state :refer [state]]
-    cljsjs.jquery
     [markdown.core :refer [md->html]]))
 
 (def caption-element (.getElementById js/document "caption"))
-
-(defn make-link-popout!
-  []
-  (this-as this
-    (.attr (js/$ this) "target" "_blank")))
 
 (defn set-caption!
   [caption]
   (aset caption-element "style" "opacity" "1")
   (let [html (md->html caption)]
     (aset caption-element "innerHTML" html))
-  (-> (js/$ caption-element)
-      (.find "a")
-      (.each make-link-popout!)))
+
+  ;; make all links open in new tab
+  (let [elements (.querySelectorAll caption-element "a")]
+    (dotimes [i (aget elements "length")]
+      (let [el (aget elements i)]
+        (.setAttribute el "target" "_blank")))))
 
 (defn on-caption-change
   [_key _atom {old-caption :caption} {new-caption :caption}]
